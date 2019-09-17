78395c562aa432a2460c810f9bdbeb9ddea36d43
Martin Hristov <martinkrhristov@gmail.com>
2019-09-17 14:07:28 +0300

    adding part of exercise of ch1

---
+++ Chapter01/os_args/echo4.go
@@ -0,0 +1,15 @@
+// echo with range func
+// echo program that prints the command used to run it
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"path"
+)
+
+func main() {
+	fmt.Println(strings.Join(os.Args[1:], " "))
+	fmt.Println("The name of the command is :", path.Base(os.Args[0]))
+}
