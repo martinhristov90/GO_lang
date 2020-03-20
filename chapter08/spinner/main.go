126b6fde52057a6973263d1f0f6b6d867e390954
Martin Hristov <mhristov@hashicorp.com>
2020-03-20 13:00:14 +0200

    adding exercise of ch08

---
+++ chapter08/spinner/main.go
@@ -0,0 +1,29 @@
+package main
+// Example form gopl
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go spinner(100 * time.Millisecond)
+	const n = 458
+	fibN := fib(n) // slow
+	fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
+}
+
+func spinner(delay time.Duration) {
+	for {
+		for _, r := range `-\|/` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(delay)
+		}
+	}
+}
+
+func fib(x int) int {
+	if x < 2 {
+		return x
+	}
+	return fib(x-1) + fib(x-2)
+}
